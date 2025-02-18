00a5c30ffac20784d50dbe8b7361c494d30fae00
AlexandruPilca <alexandru.pilca@codingmonkeys.net>
2025-02-18 15:07:13 +0100

    create project to do list js

---
+++ To do List Project/to_do_list.js
@@ -0,0 +1,24 @@
+function addTask(){
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value === ""){
+         return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = taskInput.value;
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "X"
+    deleteButton.addEventListener ("click", function(){
+        li.remove();
+        
+    });
+
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+    taskInput.value ="";
+    
+};
+
