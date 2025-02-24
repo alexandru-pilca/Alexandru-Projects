0513746ace2fe24528d8749eb90ff93d7abd9044
AlexandruPilca <alexandru.pilca@codingmonkeys.net>
2025-02-24 22:45:11 +0100

    upd weather app

--- Weather app/script.js
+++ Weather app/script.js
@@ -11,34 +11,41 @@ async function checkWeather(city) {
     let response = await fetch(
       weatherApp.apiUrl + city + `&appid=${weatherApp.apiKey}`
     );
-    let data = await response.json();
-
-    if (response.status == 404 || data.cod === "404") {
+    if (response.status == 404) {
       document.querySelector(".error").style.display = "block";
       document.querySelector(".weather").style.display = "none";
-      return;
+    } else {
+      let data = await response.json();
+
+      console.log(data);
+
+      document.querySelector(".city").innerHTML = data.name;
+      document.querySelector(".temp").innerHTML =
+        Math.round(data.main.temp) + "°C";
+      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+      document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+      let currentTime = data.dt; 
+      let sunriseTime = data.sys.sunrise; 
+      let sunsetTime = data.sys.sunset; 
+      let isNight = currentTime < sunriseTime || currentTime > sunsetTime;
+
+      if (data.weather[0].main === "Clouds") {
+        weatherApp.weatherIcon.src = isNight
+          ? "images/cloudy (1).png"
+          : "images/cloudy.png";
+      } else if (data.weather[0].main === "Clear") {
+        weatherApp.weatherIcon.src = isNight ? "images/crescent-moon.png" : "images/sun.png";
+      } else if (data.weather[0].main === "Snow") {
+        weatherApp.weatherIcon.src = isNight ? "images/snow (1).png" : "images/snow.png";
+      } else if (data.weather[0].main === "Rain") {
+        weatherApp.weatherIcon.src = isNight ? "images/thunder.png" : "images/storm.png";
+      }else if (data.weather[0].main === "Mist") {
+        weatherApp.weatherIcon.src = isNight ? "images/mist.png" : "images/mist.png";
+      }
+      document.querySelector(".weather").style.display = "block";
+      document.querySelector(".error").style.display = "none";
     }
-
-    console.log(data);
-
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML =
-      Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-
-    if (data.weather[0].main === "Clouds") {
-      weatherApp.weatherIcon.src = "images/cloud.png";
-    } else if (data.weather[0].main === "Clear") {
-      weatherApp.weatherIcon.src = "images/sun.png";
-    } else if (data.weather[0].main === "Snow") {
-      weatherApp.weatherIcon.src = "images/snow.png";
-    } else if (data.weather[0].main === "Rain") {
-      weatherApp.weatherIcon.src = "images/rain.png";
-    }
-
-    document.querySelector(".weather").style.display = "block";
-    document.querySelector(".error").style.display = "none";
   } catch (error) {
     console.error("Error fetching weather data:", error);
   }
